45df53aef390b801f714415878369d241765be86
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2023-09-29 22:00:38 +0300

    add cors settings

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+
+const usersRouter = require('./routes/api/users');
+const addressesRouter = require('./routes/api/addresses');
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/users', usersRouter);
+app.use('/api/addresses', addressesRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Page with this url was not found' })
+})
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
