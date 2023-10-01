292b0d7ecba67a89f07381b48f4a7a3999ad5410
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2023-10-01 22:40:44 +0300

    update app

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const db = require('./server');
+const cors = require('cors');
+// const db = require('./server');
 const usersRouters = require('./routes/users');
+const addressesRouters = require('./routes/addresses');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/users', usersRouters);
+app.use('/addresses', addressesRouters);
 
+app.use((req, res) => {
+  res.status(404).json({ message: 'Page with this url was not found' })
+})
 
 app.listen(3000, () => console.log('App listening on port 3000'));
